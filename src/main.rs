a686088fca39d52377c4b25760e6325c44182a6a
MA-Thomas <35780950+MA-Thomas@users.noreply.github.com>
2024-08-12 16:17:59 -0400

    refactoring complete

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use actix_web::{App, HttpServer};
 use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
 
-use h_bank::contracts::cohorts::public_api::{
-    AppState, configure_app, DataManager, ExecutionEngine, ArchiveSystem, ResultProcessor, JobStatus
+use h_bank::api::{
+    public_api::configure_app,
+    data_manager::DataManager,
+    execution_engine::ExecutionEngine,
+    archive_system::ArchiveSystem,
+    result_processor::ResultProcessor,
 };
+
+// Assuming AppState and JobStatus are defined in one of the api submodules
+use h_bank::api::models::{AppState, JobStatus};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Initialize components
     let data_manager = Arc::new(DataManager::new());
     let execution_engine = Arc::new(Mutex::new(ExecutionEngine::new()));
     let archive_system = Arc::new(ArchiveSystem::new());
     let result_processor = Arc::new(ResultProcessor::new());
     let jobs = Arc::new(Mutex::new(HashMap::new()));
 
-    // Create shared state
     let state = actix_web::web::Data::new(AppState {
         data_manager,
         execution_engine,
@@ -24,7 +29,6 @@ async fn main() -> std::io::Result<()> {
         jobs,
     });
 
-    // Set up and run the server
     println!("Starting server at http://127.0.0.1:8080");
     HttpServer::new(move || {
         App::new()
